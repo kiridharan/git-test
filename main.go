a3e7eb022d0af025092bd82d1b0fed68347d6b0d
Kiridharan <Kiridharan217dec2001@gmail.com>
2023-11-04 14:52:07 +0530

    old build

--- main.go
+++ main.go
@@ -1,26 +1,92 @@
 package main
 
 import (
-	// `"github.com/gin-gonic/gin"` is importing the Gin package from the GitHub repository
-	// `gin-gonic/gin`. Gin is a web framework for Go that provides a set of tools and utilities for
-	// building web applications.
-	"github.com/gin-gonic/gin"
+	"encoding/json"
+	"html/template"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+// Todo represents a task in the todo list.
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+}
+
+var todos []Todo
+
 func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", index).Methods("GET")
+	r.HandleFunc("/todos", getTodos).Methods("GET")
+	r.HandleFunc("/todos", createTodo).Methods("POST")
 
-	// Set /api/v1 as the base route
-	r := gin.Default()
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
+}
 
-	// Set the route group
+func index(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.New("index").Parse(`
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Todo List Test</title>
+</head>
+<body>
+    <h1>Todo List</h1>
+    <form id="todoForm" action="/todos" method="post">
+        <input type="text" name="title" placeholder="Enter a new todo" />
+        <button type="submit">Add</button>
+    </form>
+    
+    <h2>New Todos:</h2>
+    <ul id="newTodos">
+        {{range .}}
+        <li>{{.Title}}</li>
+        {{end}}
+    </ul>
+    <script>
+        document.getElementById("todoForm").addEventListener("submit", function(event) {
+            event.preventDefault();
+            const input = document.querySelector("input[name='title']");
+            const title = input.value;
+            
+            fetch("/todos", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ title: title })
+            })
+            .then(response => response.json())
+            .then(todo => {
+                input.value = ""; // Clear the input field
+                const ul = document.getElementById("newTodos");
+                const li = document.createElement("li");
+                li.textContent = todo.title;
+                ul.appendChild(li);
+            });
+        });
+    </script>
+</body>
+</html>
+`))
+	tmpl.Execute(w, todos)
+}
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Welcome to the API",
-		})
-	})
+func getTodos(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todos)
+}
 
-	// Call the scheduler.PulData() function here
+func createTodo(w http.ResponseWriter, r *http.Request) {
+	var todo Todo
+	_ = json.NewDecoder(r.Body).Decode(&todo)
+	todo.ID = len(todos) + 1
+	todos = append(todos, todo)
 
-	r.Run(":" + "3000")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todo)
 }
